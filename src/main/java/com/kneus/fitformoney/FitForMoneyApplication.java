5196d1a0dd2cf49575b9b99f430f82b1e152c5c2
Huijser <edwin.huijser@ing.com>
2023-02-09 14:53:53 +0100

    Initial commit of source project

---
+++ src/main/java/com/kneus/fitformoney/FitForMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.kneus.fitformoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitForMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitForMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kneus/fitformoney/FitForMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kneus.fitformoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitForMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
